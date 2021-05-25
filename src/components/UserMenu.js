2b6f7ce2172849e033221a51966f02ff311e1c32
TMFV <denys.tymofieiev@gmail.com>
2021-05-25 14:35:58 +0300

    Create UserMenu.js

---
+++ src/components/UserMenu.js
@@ -0,0 +1,25 @@
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  mail: {
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: 700,
+    marginRight: 20,
+  },
+};
+
+const UserMenu = ({ mail, name, onLogout }) => (
+  <div style={styles.container}>
+    <span style={styles.mail}>User e-mail: {mail}</span>
+    <span style={styles.name}>User name: {name}</span>
+    <button type="button" onClick={onLogout}>
+      Logout
+    </button>
+  </div>
+);
+
+export default UserMenu;
