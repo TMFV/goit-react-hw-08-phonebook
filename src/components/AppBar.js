0e4e746e2af63a5a99bd2fa16eb9bab6be834ea2
TMFV <denys.tymofieiev@gmail.com>
2021-05-25 14:20:14 +0300

    add AppBar

---
+++ src/components/AppBar.js
@@ -0,0 +1,21 @@
+import AuthNav from "./AuthNav";
+import UserMenu from "./UserMenu";
+import Navigation from "./Navigation";
+
+const styles = {
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottom: "1px solid #2A363B",
+  },
+};
+
+const AppBar = ({ isAuthenticated }) => (
+  <header style={styles.header}>
+    <Navigation />
+    {isAuthenticated ? <UserMenu /> : <AuthNav />}
+  </header>
+);
+
+export default AppBar;
